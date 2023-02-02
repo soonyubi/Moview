e17b563933e506b37583f82f4d496d388dab2bf4
HongSoonYeop <46443443+soonyubi@users.noreply.github.com>
2023-02-02 16:31:12 +0900

    add test code (#2)

--- user/application/user-service.test.js
+++ user/application/user-service.test.js
@@ -3,7 +3,7 @@ describe("UserService",()=>{
     describe("SignIn",()=>{
 
         test("validate user inputs",()=>{
-
+            // some thing that test code
         });
 
         test("validate response",()=>{
